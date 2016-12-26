d282f7a3935a5cb553ad8364fb6fd6c3b4f8ff72
Neoakb <neoakb@gmail.com>
2016-12-26 01:34:19 -0500

    test
    test

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	
+	public int maxGuessesAllowed = 10;
+	public Text text;
+	
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame(){
+		max = 1000;
+		min = 1;
+		NextGuess();
+	}
+	
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+	}
+	
+	void NextGuess(){
+		guess = Random.Range(min,max+1)/2;
+		text.text = guess.ToString();
+		maxGuessesAllowed--;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
